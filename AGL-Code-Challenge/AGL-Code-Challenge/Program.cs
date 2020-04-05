d6ad17398c0d374bd846a7fec5d0327ac019a472
zeeshan_mcad@hotmail.com <zeeshan_mcad@hotmail.com>
2020-04-06 08:23:12 +1000

    Exception handling

--- AGL-Code-Challenge/AGL-Code-Challenge/Program.cs
+++ AGL-Code-Challenge/AGL-Code-Challenge/Program.cs
@@ -4,6 +4,7 @@ using RestSharp;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using static AGL_Code_Challenge.BusinessModels.Enum;
@@ -14,37 +15,65 @@ namespace AGL_Code_Challenge
     {
         static void Main(string[] args)
         {
-            var data = GetJsonData("http://agl-developer-test.azurewebsites.net/people.json");
+            try
+            {
+                var data = GetJsonData("http://agl-developer-test.azurewebsites.net/people.json");
 
-           var maleCatList =  BusinessLogic.GetCatsNameByGender(data, GenderEnum.Male.ToString(), AnimalEnum.Cat.ToString());
-           var femaleCatList = BusinessLogic.GetCatsNameByGender(data, GenderEnum.Female.ToString(), AnimalEnum.Cat.ToString());
+                var maleCatList = BusinessLogic.GetCatsNameByGender(data, GenderEnum.Male.ToString(), AnimalEnum.Cat.ToString());
+                var femaleCatList = BusinessLogic.GetCatsNameByGender(data, GenderEnum.Female.ToString(), AnimalEnum.Cat.ToString());
 
-            PrintCatList(data, maleCatList, GenderEnum.Male.ToString());
-            PrintCatList(data, femaleCatList, GenderEnum.Female.ToString());
-
-
+                PrintCatList(data, maleCatList, GenderEnum.Male.ToString());
+                PrintCatList(data, femaleCatList, GenderEnum.Female.ToString());
+            }
+            catch (Exception ex)
+            {
+                if (ex is WebException)
+                {
+                    var e = (WebException)ex;
+                    Console.WriteLine(ex.Message);                    
+                }
+                else
+                {
+                    Console.Write(ex.Message);
+                }
+            }
             Console.ReadLine();
 
         }
-        private static void PrintCatList(List<People> pplList,List<Pet> pets,string gender)
+        private static void PrintCatList(List<People> pplList, List<Pet> pets, string gender)
         {
-            Console.WriteLine("------------------");
-            Console.WriteLine(pplList.Where(g=>g.Gender == gender).FirstOrDefault().Gender);
-            Console.WriteLine("------------------");
-
-            foreach (var cat in pets)
+            if (pplList.Any())
             {
-                Console.WriteLine(cat.Name);
+                Console.WriteLine("------------------");
+                Console.WriteLine(pplList.Where(g => g.Gender == gender).FirstOrDefault().Gender);
+                Console.WriteLine("------------------");
+
+                foreach (var cat in pets)
+                {
+                    Console.WriteLine(cat.Name);
+                }
+            }
+            else
+            {
+                Console.WriteLine("List count is  0");
             }
         }
-        
+
         public static List<People> GetJsonData(string url)
         {
+            List<People> list = new List<People>();
             var client = new RestClient(url);
 
             var response = client.Execute<List<People>>(new RestRequest());
-           
-            return response.Data;
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                list = response.Data;
+            }
+            else
+            {
+                throw response.ErrorException;
+            }
+            return list;
         }
     }
 }
